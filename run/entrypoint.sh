e7446ad9b7d6a6ee9444c7572171cf366bb3443a
cmd <cmd@yggdrasil.onion>
2022-05-27 04:44:15 -0500

    refactored the method for attaching containers

--- run/entrypoint.sh
+++ run/entrypoint.sh
@@ -13,27 +13,17 @@ IND=`fgc 215 "|-"`
 # Methods
 ###############################################################################
 
-safe_greeting() {
-  printf "Press '$(fgc 220 $ESC_KEYS)' to detatch from the terminal. Your node will continue to run in the background.
-You can re-enter this terminal with the command '$(fgc 220 "docker exec -it $HOSTNAME bash")'.\n\n"
-}
-
-dev_greeting() {
-  printf "Now running in dev-mode. Type '$(fgc 220 exit)' to quit this session and terminate the node.
-  If you experience any issues, use the '$(fgc 220 start-node)' command to re-run this startup script.\n\n"
-}
-
-finish() {
+clean_exit() {
   status="$?"
-  [ $status -ne 0 ] && printf "\nFailed with exit code $state" && templ fail
-  [ -z "$DEVMODE" ] && cleanup || exit 0
+  [ $status -ne 0 ] && ( printf "\nFailed with exit code $state"; templ fail )
+  [ $status -ne 0 ] && [ -z "$DEVMODE" ] && cleanup || exit 0
 }
 
 cleanup() {
   if [ -z "$DEVMODE" ]; then
     printf "Delisting $SHAREPATH/$HOSTNAME ... "
     rm -rf "$SHAREPATH/$HOSTNAME"
-    printf "done. " && exit 0
+    printf "done.\n" && exit 2
   fi
 }
 
@@ -41,10 +31,10 @@ cleanup() {
 # Script
 ###############################################################################
 
-trap finish EXIT; trap cleanup SIGTERM SIGKILL
+trap clean_exit EXIT; trap cleanup SIGTERM SIGKILL
 
-## Add a little delay for docker to attach the tty properly.
-if [ -z "$DEVMODE" ]; then sleep 1; fi
+[ -z "$DEVMODE" ] && sleep 1   ## Add some delay for docker to attach tty properly.
+[ -n "$STARTUP" ] && startup   ## Launch startup script.
 
 ## Execute startup scripts.
 for script in `find $RUNPATH/startup -name *.sh | sort`; do
@@ -52,25 +42,8 @@ for script in `find $RUNPATH/startup -name *.sh | sort`; do
   if [ $state -ne 0 ]; then exit $state; fi
 done
 
-## Print a fancy banner depending on startup success / failure.
-if [ $? -ne 0 ]; then 
-  templ banner "Node startup failed!" && exit 1
-else
+## Print a failure banner if we fail.
+[ $? -ne 0 ] && ( templ banner "Node startup failed!"; exit 1 )
 
-  templ banner "$HOSTNAME is initialized!"
-  ip_addr=`ip -f inet addr show eth0 | grep -Po 'inet \K[\d.]+'`
-  spark_key=`cat /data/lightning/sparko.keys | kgrep STREAM_KEY`
-  spark_user=`cat /data/lightning/sparko.login | kgrep USERNAME`
-  spark_pass=`cat /data/lightning/sparko.login | kgrep PASSWORD`
-  stream_uri="stream?access-key=$(printf $spark_key | awk -F ':' '{ print $1 }')"
-
-  printf "$(tput bold)Node ID:$(tput sgr0)      $(lightning-cli getinfo | jgrep id)\n"
-  printf "$(tput bold)Stream Link:$(tput sgr0)  $(fgc 033 "http://$ip_addr:9737/$stream_uri")\n\n"
-  printf "$(tput bold)Wallet Link:$(tput sgr0)  $(fgc 033 "http://$ip_addr:9737")\n"
-  printf "$(tput bold)Wallet Login:$(tput sgr0) $spark_user // $spark_pass\n"
-  echo
-
-fi
-
-## Greet user and setup terminal session.
-if [ -z "$DEVMODE" ]; then safe_greeting; /bin/bash; else dev_greeting; fi
+## Start terminal service.
+terminal
