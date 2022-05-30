eb34a022d8fe14a6626f16a542dd9ce731e2b078
cmd <cmd@yggdrasil.onion>
2022-05-29 20:32:08 -0500

    added auto-peering to faucet

--- run/startup/02-bitcoin-setup.sh
+++ run/startup/02-bitcoin-setup.sh
@@ -110,8 +110,8 @@ fi
 # Peer Connection
 ###############################################################################
 
-if ( [ -n "$PEER_LIST" ] || [ -n "$CHAN_LIST" ] ); then
-  for peer in $(printf $PEER_LIST $CHAN_LIST | tr ',' ' '); do
+if ( [ -n "$PEER_LIST" ] || [ -n "$CHAN_LIST" ] || [ -n "$USE_FAUCET" ]); then
+  for peer in $(printf $PEER_LIST $CHAN_LIST $USE_FAUCET | tr ',' ' '); do
     
     ## Search for peer file in peers path.
     echo && printf "Checking connection to $peer: "
