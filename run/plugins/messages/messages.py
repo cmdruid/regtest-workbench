c389e75c03612ba722a33d719815c5ca6c4dfc2a
cmd <cmd@yggdrasil.onion>
2022-06-01 15:36:07 -0500

    refactored messages

--- run/plugins/messages/messages.py
+++ run/plugins/messages/messages.py
@@ -3,53 +3,53 @@
 ## Uses methods, hooks, subscriptions and custom notifications.
 
 from pyln.client import Plugin
-
-import random, sys
+import random
 
 plugin = Plugin()
 
-@plugin.method('sendmessage')
-def send_msg(plugin, peer_id, payload):
-  """Messages are in the byte format of [type(2)][id(8)][data]."""
-  msgtype = int(plugin.get_option('mtype'), 16)
-  msgid   = random.randint(0, 2**64)
-  msg     = (msgtype.to_bytes(2, 'big')
-            + msgid.to_bytes(8, 'big')
-            + bytes(payload, encoding='utf8'))
-  res = plugin.rpc.call('sendcustommsg', {'node_id': peer_id, 'msg': msg.hex()})
-  return "Message sent: {}".format(res)
+
+# @plugin.method('sendmessage')
+# def send_msg(plugin, peer_id, payload):
+#   """Messages are in the byte format of [type(2)][id(8)][data]."""
+#   msgtype = int(plugin.mtype, 16)
+#   msgid   = random.randint(0, 2**64)
+#   msg     = (msgtype.to_bytes(2, 'big')
+#             + msgid.to_bytes(8, 'big')
+#             + bytes(payload, encoding='utf8'))
+#   res = plugin.rpc.call('sendcustommsg', {'node_id': peer_id, 'msg': msg.hex()})
+#   return res
 
 
-@plugin.async_hook('custommsg')
-def on_custommsg(peer_id, payload, plugin, **kwargs):
-  """Use custommsg hook to receive payload."""
-  pbytes  = bytes.fromhex(payload)
-  mtype   = int.from_bytes(pbytes[:2], "big")
-  msgid   = int.from_bytes(pbytes[2:10], "big")
-  data    = pbytes[10:].decode()
-  payload = dict({ 'peer': peer_id, 'mtype': mtype, 'msgid': msgid, 'data': data })
+# @plugin.async_hook('custommsg')
+# def on_custommsg(peer_id, payload, plugin, **kwargs):
+#   """Use custommsg hook to receive payload."""
+#   pbytes  = bytes.fromhex(payload)
+#   mtype   = int.from_bytes(pbytes[:2], "big")
+#   plugin.log('Received payload type: {}'.format(hex(mtype)))
+#   if hex(mtype) === plugin.mtype.lower():
+#     msgid   = int.from_bytes(pbytes[2:10], "big")
+#     data    = pbytes[10:].decode()
+#     message = dict({ 'peer': peer_id, 'msgid': msgid, 'data': data })
+#     plugin.notify("message", message)
 
-  plugin.notify("newmessage", payload)
-
-  return {'result': 'continue'}
+#   return {'result': 'continue'}
 
 
-@plugin.subscribe('newmessage')
-def on_newmessage(plugin, origin, payload, **kwargs):
-  """Custom notifications include an origin and payload argument."""
-  response = "Received new message from {}: {}".format(origin, payload)
-  plugin.log(response)
-  sys.stdout.write(response)
+# @plugin.subscribe('message')
+# def on_newmessage(plugin, origin, payload, **kwargs):
+#   """Custom notifications include an origin and payload argument."""
+#   plugin.log("Received message: {}".format(payload))
+#   return payload
 
 
 @plugin.init()
 def init(options, configuration, plugin):
-  """This can also return {'disabled': <reason>} to self-disable."""
-  plugin.log("Plugin messages initialized.")
+  #plugin.mtype = plugin.get_option('mtype')
+  plugin.log("Plugin messages.py initialized.")
   return
 
 
-plugin.add_notification_topic('newmessage')
-plugin.add_option('mtype', '0xFFFF', 'Set the message type, in hex. Default 0xFFFF')
+plugin.add_notification_topic('message')
+plugin.add_option('mtype', '0xFFFF', 'Set the message type, in hex. Must be odd number. Default 0xFFFF')
 
-plugin.run()
+plugin.run() ## test 1 2 3 4sadasdasdasdasd
